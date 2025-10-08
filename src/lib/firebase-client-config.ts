0fad27069ac4dbd5bf51229f4156100ea047cde5
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-08 09:05:35 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase: Error (a

--- src/lib/firebase-client-config.ts
+++ src/lib/firebase-client-config.ts
@@ -1,5 +1,4 @@
 
-
 // This file is the single source of truth for the client-side Firebase configuration.
 // It is used by the application to connect to Firebase services from the browser.
 
@@ -7,13 +6,12 @@ export const getFirebaseConfig = () => {
     // These variables will be set by Firebase App Hosting during the build process
     // based on the active configuration.
     const firebaseConfig = {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+      apiKey: "AIzaSyCtlF3onBhtwg0Hh3iOOEjygi9mj81wxrA",
+      authDomain: "fly4all-78277122-3cbd0.firebaseapp.com",
+      projectId: "fly4all-78277122-3cbd0",
+      storageBucket: "fly4all-78277122-3cbd0.appspot.com",
+      messagingSenderId: "108505683067",
+      appId: "1:108505683067:web:3ab7755349630154e77ede"
     };
 
     if (!firebaseConfig.projectId) {
