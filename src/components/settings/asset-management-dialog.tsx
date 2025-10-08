6915080997a6a83e7e840c42455f5fd287a05c33
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-08 18:40:28 +0000

    قم بالغاء كامل الملحقات ايضا

--- src/components/settings/asset-management-dialog.tsx
+++
@@ -1,6 +0,0 @@
-
-// This component is deprecated. The Asset Management content is now displayed directly
-// in the appearance settings tab. This file will be removed in a future cleanup.
-export default function DeprecatedAssetManagementDialog() {
-    return null;
-}
--- src/components/settings/chart-of-accounts-dialog.tsx
+++
@@ -1,6 +0,0 @@
-
-// This component is deprecated. The Chart of Accounts is now displayed directly
-// in the accounting settings tab. This file will be removed in a future cleanup.
-export default function DeprecatedChartOfAccountsDialog() {
-    return null;
-}
