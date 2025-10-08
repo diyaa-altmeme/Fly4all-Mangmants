b99c395e4e6d86a8a9d582274a87246b80e0a566
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-08 19:00:20 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCtlF3onBhtwg0Hh3iOOEjygi9mj81w

--- src/lib/firebase-client-config.ts
+++ src/lib/firebase-client-config.ts
@@ -1,16 +1,16 @@
+
 // This file is the single source of truth for the client-side Firebase configuration.
 // It is used by the application to connect to Firebase services from the browser.
 
 export const getFirebaseConfig = () => {
     // These values are now hardcoded to ensure correctness.
     const firebaseConfig = {
-      apiKey: "AIzaSyBNZ8ZJKKZJKKZJKKZJKKZJKKZJKKZJKK",
-      authDomain: "fly4all-mangmants-go-591-d7ffe.firebaseapp.com",
-      projectId: "fly4all-mangmants-go-591-d7ffe",
-      storageBucket: "fly4all-mangmants-go-591-d7ffe.appspot.com",
-      messagingSenderId: "1234567890123",
-      appId: "1:1234567890123:web:test123456789",
-      measurementId: "G-TEST123456"
+      apiKey: "AIzaSyCtlF3onBhtwg0Hh3iOOEjygi9mj81wxrA",
+      authDomain: "fly4all-78277122-3cbd0.firebaseapp.com",
+      projectId: "fly4all-78277122-3cbd0",
+      storageBucket: "fly4all-78277122-3cbd0.appspot.com",
+      messagingSenderId: "108505683067",
+      appId: "1:108505683067:web:3ab7755349630154e77ede"
     };
 
     if (!firebaseConfig.projectId) {
