4227a2f7f3af851700c2cb1bbfad9681687887c1
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-08 20:51:04 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCtlF3onBhtwg0Hh3iOOEjygi9mj81w

--- src/lib/firebase-client-config.ts
+++ src/lib/firebase-client-config.ts
@@ -8,7 +8,7 @@ export const getFirebaseConfig = () => {
       apiKey: "AIzaSyCtlF3onBhtwg0Hh3iOOEjygi9mj81wxrA",
       authDomain: "fly4all-78277122-3cbd0.firebaseapp.com",
       projectId: "fly4all-78277122-3cbd0",
-      storageBucket: "fly4all-78277122-3cbd0.appspot.com",
+      storageBucket: "fly4all-78277122-3cbd0.firebasestorage.app",
       messagingSenderId: "108505683067",
       appId: "1:108505683067:web:3ab7755349630154e77ede"
     };
