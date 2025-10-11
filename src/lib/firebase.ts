a6b95f44847e7f0838782fb6cf8c7c5e408e6f77
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-11 17:55:32 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/net

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth, Auth } from 'firebase/auth';
 
 // معلومات مشروعك في Firebase
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBNZ8ZJKKZJKKZJKKZJKKZJKKZJKKZJKK",
+  authDomain: "fly4all-mangmants-go-591-d7ffe.firebaseapp.com",
+  projectId: "fly4all-mangmants-go-591-d7ffe",
+  storageBucket: "fly4all-mangmants-go-591-d7ffe.appspot.com",
+  messagingSenderId: "1234567890123",
+  appId: "1:1234567890123:web:test123456789",
+  measurementId: "G-TEST123456"
 };
 
 // تهيئة Firebase (نتحقق إذا كان مهيأ مسبقاً لتجنب الأخطاء)
