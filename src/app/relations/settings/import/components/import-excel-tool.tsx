0c2e778bd48c1a45d7ece46d4226b509159ecf71
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-15 06:39:27 +0000

    قم باجراء فحص وازل كل الاشياء الزائدة والتفاصيل الغير مستخدمة لتنظيم الم

--- src/app/relations/settings/import/components/import-excel-tool.tsx
+++
@@ -1,7 +0,0 @@
-
-// This component is deprecated and its functionality merged into the DynamicImportTool.
-// This file can be safely removed in a future cleanup.
-
-export default function DeprecatedImportExcelTool() {
-    return null;
-}
