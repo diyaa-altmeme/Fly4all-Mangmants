508f2ab8d6305846e1d14125e91c0652f7f8dc97
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-19 10:38:48 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCtlF3onBhtwg0Hh3iOOEjygi9mj81w

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,15 +6,14 @@ import { getDatabase, Database } from 'firebase/database';
 
 // معلومات مشروعك في Firebase
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCtlF3onBhtwg0Hh3iOOEjygi9mj81wxrA",
+  authDomain: "fly4all-78277122-3cbd0.firebaseapp.com",
+  projectId: "fly4all-78277122-3cbd0",
+  storageBucket: "fly4all-78277122-3cbd0.appspot.com",
+  messagingSenderId: "108505683067",
+  appId: "1:108505683067:web:3ab7755349630154e77ede",
   // Add the databaseURL for Realtime Database
-  databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`
+  databaseURL: "https://fly4all-78277122-3cbd0.firebaseio.com"
 };
 
 // تهيئة Firebase (نتحقق إذا كان مهيأ مسبقاً لتجنب الأخطاء)
