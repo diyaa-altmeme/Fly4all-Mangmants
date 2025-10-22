627561616b4d68aa12168172608d8d0c49b4a51d
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-22 08:37:14 +0000

    نحتاج الآن إلى البدء في **بناء وتفعيل نماذج التعديل (Edit Forms)** لكل ا

--- src/app/accounts/remittances/components/add-remittance-dialog.tsx
+++ src/app/accounts/remittances/components/add-remittance-dialog.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
