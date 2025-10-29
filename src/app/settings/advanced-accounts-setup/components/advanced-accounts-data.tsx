92afada88789d6d3b6a2cb7f45ca8c38b303804c
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-29 14:11:19 +0000

    Try fixing this error: `Runtime Error: An unknown Component is an async

--- src/app/settings/advanced-accounts-setup/components/advanced-accounts-data.tsx
+++ src/app/settings/advanced-accounts-setup/components/advanced-accounts-data.tsx
@@ -1,8 +1,6 @@
 
 import { getChartOfAccounts, getFinanceAccountsMap } from "../actions";
 import AdvancedAccountsContent from "./advanced-accounts-content";
-import type { FinanceAccountsMap } from "@/lib/types";
-import ProtectedPage from "@/components/auth/protected-page";
 
 export default async function AdvancedAccountsData() {
     const [accountsData, financeMapData] = await Promise.all([
@@ -11,8 +9,6 @@ export default async function AdvancedAccountsData() {
     ]);
     
     return (
-        <ProtectedPage permission="settings:finance:manage">
-            <AdvancedAccountsContent accounts={accountsData} financeAccountsMap={financeMapData} />
-        </ProtectedPage>
+        <AdvancedAccountsContent accounts={accountsData} financeAccountsMap={financeMapData} />
     );
 }
