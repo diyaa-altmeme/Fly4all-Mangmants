e78c5173758c602a589366ebf533b2279de18c7d
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-10-30 07:24:01 +0000

    Try fixing this error: `Runtime Error: React.Children.only expected to r

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -254,7 +254,7 @@ Sidebar.displayName = "Sidebar"
 const SidebarTrigger = React.forwardRef<
   React.ElementRef<typeof Button>,
   React.ComponentProps<typeof Button>
->(({ className, onClick, ...props }, ref) => {
+>(({ className, onClick, children, ...props }, ref) => {
   const { toggleSidebar } = useSidebar()
 
   return (
@@ -270,7 +270,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      {children || <PanelLeft />}
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
