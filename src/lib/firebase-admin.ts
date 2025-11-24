a15305604715122d76dfe0c535d4d08158e72525
حسابات الروضتين <acc.alrwdaten@gmail.com>
2025-11-24 07:43:36 +0000

    داخل مجلد:
    ```
    src/lib/firebase
    ```
    
    يوجد فعلاً الملف:
    
    ```
    firebase-ad

---
+++ src/lib/firebase-admin.ts
@@ -0,0 +1,4 @@
+// Bridge file to maintain compatibility with older imports.
+// Redirect all firebase-admin imports to the new firebase-admin-sdk file.
+
+export * from "./firebase/firebase-admin-sdk";
